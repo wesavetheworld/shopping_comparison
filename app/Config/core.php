50614ae49310cbf3ef3aa0d6d576e992fbefa1ab
Jessie <jessiepuls@gmail.com>
2014-02-22 21:50:07 -0800

    Updating config

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'aojkj:OIPOIUaj;lsja;fjjOu(*U(FjJ;fa;sdfj;a;j;q;q348tyoieyrthdfskjdfhbusdfiuy^%%^%^E@');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '85726823923489501002139057913876657768123689505');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
