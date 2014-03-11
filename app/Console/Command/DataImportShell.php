20d8c8caaa1c10ff200f172f4870b60d7d4391ef
Jessica Puls <jpuls@iowai.org>
2014-03-11 17:03:44 -0500

    Importing items, units and entries

--- app/Console/Command/DataImportShell.php
+++ app/Console/Command/DataImportShell.php
@@ -5,18 +5,18 @@ class DataImportShell extends AppShell {
 		'item',
 		'quantity',
 		'unit',
-		'unit_price',
+		'unit-price',
 		'winner',
 		'hyvee',
 		'aldi',
 		'dahls',
-		'trader_joes',
+		'trader-joes',
 		'costco',
-		'sams_club',
+		'sams-club',
 		'target',
 		'walgreens',
 		'walmart',
-		'whole_foods'
+		'whole-foods'
 	);
 
 	public function main() {
@@ -41,13 +41,9 @@ class DataImportShell extends AppShell {
 		$this->out("\n----------------- Importing Categories -----------------");
 		$price_sheet = $this->getPriceSheet();
 		$category = ClassRegistry::init('Category');
-		$category_column = array_search('category', $this->columns);
+		$category_column = $this->getColumnIndex('category');
 		$category_list = array();
 		$categories = array();
-
-		if($category_column === false) {
-			throw new Exception("Unknown column: category");
-		}
 
 		foreach ($price_sheet as $row) {
 			$category->create();
@@ -77,7 +73,7 @@ class DataImportShell extends AppShell {
 				'name' => 'Dahl\'s',
 			),
 			4  => array(
-				'slug' => 'trader_joes',
+				'slug' => 'trader-joes',
 				'name' => 'Trader Joe\'s',
 			),
 			5  => array(
@@ -85,7 +81,7 @@ class DataImportShell extends AppShell {
 				'name' => 'Costco',
 			),
 			6  => array(
-				'slug' => 'sams_club',
+				'slug' => 'sams-club',
 				'name' => 'Sam\'s Club',
 			),
 			7  => array(
@@ -101,176 +97,131 @@ class DataImportShell extends AppShell {
 				'name' => 'WalMart',
 			),
 			10 => array(
-				'slug' => 'whole_foods',
+				'slug' => 'whole-foods',
 				'name' => 'Whole Foods',
 			),
 		);
 
-		$category = ClassRegistry::init('Store');
-
-		$category->saveAll($stores);
-		/**
-		INSERT INTO stores
-			(id, name,location,created,modified)
-		VALUES
-			(1, "Aldi","",NOW(),NOW()),
-			(2, "Costco","",NOW(),NOW()),
-			(3, "Dahl's","",NOW(),NOW()),
-			(4, "HyVee","",NOW(),NOW()),
-			(5, "Sam's Club","",NOW(),NOW()),
-			(6, "Target","",NOW(),NOW()),
-			(7, "Trader Joe's","",NOW(),NOW()),
-			(8, "Walgreens","",NOW(),NOW()),
-			(9, "Walmart","",NOW(),NOW()),
-			(10, "Whole Foods","",NOW(),NOW());
-		*/
+		$store = ClassRegistry::init('Store');
+		$store->saveAll($stores);
 	}
 
 	public function items() {
-		/**
-		INSERT INTO items
-			(id, item, category_id, created, modified)
-		VALUES
-			(1, "Bread", 1, NOW(), NOW()),
-			(2, "Small Flour Tortilla", 1, NOW(), NOW()),
-			(3, "Baking Soda", 1, NOW(), NOW()),
-			(4, "Cookie Dough", 1, NOW(), NOW()),
-			(5, "Panko Bread Crumbs", 1, NOW(), NOW()),
-			(6, "Carrot Juice", 2, NOW(), NOW()),
-			(7, "K-cups", 2, NOW(), NOW()),
-			(8, "Puffins Cereal", 3, NOW(), NOW()),
-			(9, "Cereal Bars", 3, NOW(), NOW()),
-			(10, "Black Beans", 4, NOW(), NOW()),
-			(11, "Chili Beans", 4, NOW(), NOW()),
-			(12, "Bruschetta", 4, NOW(), NOW()),
-			(13, "Chicen Broth", 4, NOW(), NOW()),
-			(14, "Diced Tomatoes", 4, NOW(), NOW()),
-			(15, "Great Northern Beans", 4, NOW(), NOW()),
-			(16, "Pumpkin", 4, NOW(), NOW()),
-			(17, "Sour Kraut", 4, NOW(), NOW()),
-			(18, "Cholula Hot Sauce", 5, NOW(), NOW()),
-			(19, "Coconut Oil", 5, NOW(), NOW()),
-			(20, "Croutons", 5, NOW(), NOW()),
-			(21, "Honey", 5, NOW(), NOW()),
-			(22, "Hummus", 5, NOW(), NOW()),
-			(23, "Jelly", 5, NOW(), NOW()),
-			(24, "Maple Syrup", 5, NOW(), NOW()),
-			(25, "Parmesan Cheese", 5, NOW(), NOW()),
-			(26, "Peanut Butter", 5, NOW(), NOW()),
-			(27, "Salad Dressing", 5, NOW(), NOW()),
-			(28, "Salsa", 5, NOW(), NOW()),
-			(29, "Taco Seasoning", 5, NOW(), NOW()),
-			(30, "Almond Milk", 6, NOW(), NOW()),
-			(31, "Butter", 6, NOW(), NOW()),
-			(32, "Babybel Cheese", 6, NOW(), NOW()),
-			(33, "Dubliner Cheese", 6, NOW(), NOW()),
-			(34, "Shredded Cheddar Cheese", 6, NOW(), NOW()),
-			(35, "Coconut Milk", 6, NOW(), NOW()),
-			(36, "Cottage Cheese", 6, NOW(), NOW()),
-			(37, "Eggs", 6, NOW(), NOW()),
-			(38, "Kefir", 6, NOW(), NOW()),
-			(39, "String Cheese", 6, NOW(), NOW()),
-			(40, "Greek Yogurt", 6, NOW(), NOW()),
-			(41, "Corn", 7, NOW(), NOW()),
-			(42, "Green Beans", 7, NOW(), NOW()),
-			(43, "Hash Browns", 7, NOW(), NOW()),
-			(44, "Mixed Berries", 7, NOW(), NOW()),
-			(45, "Mixed Vegies", 7, NOW(), NOW()),
-			(46, "Pizza", 7, NOW(), NOW()),
-			(47, "Uncooked Tortillas", 7, NOW(), NOW()),
-			(48, "Dishwasher Tabs", 8, NOW(), NOW()),
-			(49, "Fabric Softener", 8, NOW(), NOW()),
-			(50, "Laundry Detergent", 8, NOW(), NOW()),
-			(51, "Toilet Paper", 8, NOW(), NOW()),
-			(52, "Trash Bags", 8, NOW(), NOW()),
-			(53, "Uncured Bacon", 9, NOW(), NOW()),
-			(54, "Boneless Skinless Chicken Breast", 9, NOW(), NOW()),
-			(55, "Chicken Sausage", 9, NOW(), NOW()),
-			(56, "Ground Beef", 9, NOW(), NOW()),
-			(57, "Grass Fed Ground Beef", 9, NOW(), NOW()),
-			(58, "Pork Shoulder", 9, NOW(), NOW()),
-			(59, "Turkey Breast", 9, NOW(), NOW()),
-			(60, "Boneless Turkey Breast", 9, NOW(), NOW()),
-			(61, "Raw Almonds", 10, NOW(), NOW()),
-			(62, "Roasted Almonds", 10, NOW(), NOW()),
-			(63, "Bulgar Wheat", 11, NOW(), NOW()),
-			(64, "Brown Rice", 11, NOW(), NOW()),
-			(65, "Dog Food", 12, NOW(), NOW()),
-			(66, "Valarian Root", 13, NOW(), NOW()),
-			(67, "Vitamins", 13, NOW(), NOW()),
-			(68, "Apple", 14, NOW(), NOW()),
-			(69, "Avocado", 14, NOW(), NOW()),
-			(70, "Bananas", 14, NOW(), NOW()),
-			(71, "Broccoli", 14, NOW(), NOW()),
-			(72, "Brussel's Sprouts", 14, NOW(), NOW()),
-			(73, "Carrots", 14, NOW(), NOW()),
-			(74, "Baby Carrots", 14, NOW(), NOW()),
-			(75, "English Seedless Cucumber", 14, NOW(), NOW()),
-			(76, "Green Onions", 14, NOW(), NOW()),
-			(77, "Lemon", 14, NOW(), NOW()),
-			(78, "Lime", 14, NOW(), NOW()),
-			(79, "Mushrooms", 14, NOW(), NOW()),
-			(80, "Onions", 14, NOW(), NOW()),
-			(81, "Clementines Oranges", 14, NOW(), NOW()),
-			(82, "Navel Oranges", 14, NOW(), NOW()),
-			(83, "Mini Sweet Peppers", 14, NOW(), NOW()),
-			(84, "Multi color Peppers", 14, NOW(), NOW()),
-			(85, "Red Skinned Potatoes", 14, NOW(), NOW()),
-			(86, "Salad", 14, NOW(), NOW()),
-			(87, "Spinach", 14, NOW(), NOW()),
-			(88, "Sweet Potatoes", 14, NOW(), NOW()),
-			(89, "Yellow Cherry Tomatoes", 14, NOW(), NOW()),
-			(90, "Gum", 15, NOW(), NOW()),
-			(91, "Turkey Jerkey", 15, NOW(), NOW()),
-			(92, "Beef Jerky", 15, NOW(), NOW()),
-			(93, "Pop Chips", 15, NOW(), NOW()),
-			(94, "Premade Popcorn", 15, NOW(), NOW()),
-			(95, "Prezel Crisps", 15, NOW(), NOW()),
-			(96, "Prezels", 15, NOW(), NOW()),
-			(97, "Garlic Powder", 16, NOW(), NOW());
-		*/
+		$this->out("\n----------------- Importing Items -----------------");
+		$price_sheet = $this->getPriceSheet();
+
+		$item = ClassRegistry::init('Item');
+		$item_column = $this->getColumnIndex('item');
+		$item_list = array();
+		$items = array();
+
+		$categories = $this->readCategories();
+		$category_column = $this->getColumnIndex('category');
+
+		foreach ($price_sheet as $row) {
+			$item->create();
+			$category_id = $this->getCategoryIdForLabel($row[$category_column]);
+
+			if(($item_name = $row[$item_column]) && array_search($row[$item_column], $item_list) === false) {
+				$category = $this->categories[$category_id];
+				$this->out("Adding $item_name with category $category");
+
+				$items[] = array(
+					'item' => $item_name,
+					'category_id' => $category_id,
+				);
+
+				$item_list[] = $item_name;
+			}
+		}
+
+		$item->saveAll($items);
 	}
 
 	public function units() {
-		/**
-		INSERT INTO units
-			(id, unit, created, modified)
-		VALUES
-			(1, "package", NOW(), NOW()),
-			(2, "serving", NOW(), NOW()),
-			(3, "item", NOW(), NOW()),
-			(4, "lb", NOW(), NOW()),
-			(5, "oz", NOW(), NOW());
-		*/
+		$this->out("\n----------------- Importing Units -----------------");
+		$price_sheet = $this->getPriceSheet();
+		$unit = ClassRegistry::init('Unit');
+		$unit_column = $this->getColumnIndex('unit');
+		$unit_list = array();
+		$units = array();
+
+		foreach ($price_sheet as $row) {
+			$unit->create();
+			if(!empty($row[$unit_column]) && array_search($row[$unit_column], $unit_list) === false) {
+				$unit_name = $row[$unit_column];
+				$this->out("Adding $unit_name");
+				$units[] = array('unit' => $unit_name);
+				$unit_list[] = $unit_name;
+			}
+		}
+
+		$unit->saveAll($units);
 	}
 
 	public function entries() {
-		/**
-		INSERT INTO entries
-			(item_id, price, quantity, unit_id, unit_price, store_id, note, created, modified)
-		VALUES
-			(1, 1.58, 1, 1, price/quantity, 4,'', NOW(), NOW()),
-			(1, 1.29, 1, 1, price/quantity, 1,'', NOW(), NOW()),
-			(1, 2.89, 1, 1, price/quantity, 3,'', NOW(), NOW()),
-			(2, 1.69, 20, 3, price/quantity, 1,'', NOW(), NOW()),
-			(2, 1.29, 10, 3, price/quantity, 3,'', NOW(), NOW()),
-			(3, 6.59, 13.5, 4, price/quantity, 2,'', NOW(), NOW()),
-			(4, 1.99, 16, 5, price/quantity, 1,'', NOW(), NOW()),
-			(5, 1.79, 7, 5, price/quantity, 7,'', NOW(), NOW()),
-			(6, 6.99, 96, 5, price/quantity, 2,'', NOW(), NOW()),
-			(7, 2.99, 7, 3, price/quantity, 7, "store-brand", NOW(), NOW()),
-			(7, 31.89, 100, 3, price/quantity, 2, "store-brand", NOW(), NOW()),
-			(7, 37.99, 80, 3, price/quantity, 2, "organic", NOW(), NOW()),
-			(8, 3.99, 16, 5, price/quantity, 7,'', NOW(), NOW()),
-			(9, 1.99, 6, 3, price/quantity, 7,'', NOW(), NOW()),
-			(10, .59, 15, 5, price/quantity, 1,'', NOW(), NOW()),
-			(10, 4.69, 180, 5, price/quantity, 2, "12 15 oz cans", NOW(), NOW()),
-			(11, .59, 15, 5, price/quantity, 1,'', NOW(), NOW()),
-			(12, 2.49, 12, 5, price/quantity, 7,'', NOW(), NOW()),
-			(13, 1.69, 32, 5, price/quantity, 1, "organic", NOW(), NOW()),
-			(14, 1.49, 28, 5, price/quantity, 1, "organic", NOW(), NOW()),
-			(14, 7.69, 116, 5, price/quantity, 2, "organic", NOW(), NOW());
-		*/
+		$this->out("\n----------------- Importing Entries -----------------");
+		$price_sheet = $this->getPriceSheet();
+		$entry = ClassRegistry::init('Entry');
+		$entries = array();
+
+		$this->readItems();
+		$item_column = $this->getColumnIndex('item');
+
+		$this->readUnits();
+		$unit_column = $this->getColumnIndex('unit');
+
+		$this->readCategories();
+		$category_column = $this->getColumnIndex('category');
+		
+		$this->readStoreSlugs();
+		$store_columns = array();
+
+		$this->readStores();
+
+		foreach($this->store_slugs as $slug) {
+			$store_columns[$slug] = $this->getColumnIndex($slug);
+		}
+
+		$unit_price_column = $this->getColumnIndex('unit-price');
+
+		foreach ($price_sheet as $row) {
+			// Bakery,Bread (store brand whole wheat),1,loaf,$1.29,Aldi,$1.58,$1.29,$2.89,p,p,,p,,,
+			$item = $row[$item_column];
+			$item_id = $this->getItemIdForLabel($item);
+			$unit_id = $this->getUnitIdForLabel($row[$unit_column]);
+			
+			foreach ($store_columns as $store_column) {
+				$unit_price = preg_replace('/[a-z$]/','',$row[$store_column]);
+
+				if(empty($unit_price)) {
+					continue;
+				}
+
+				if(empty($unit_price)) {
+					continue;
+				}
+
+				$quantity = 1; // Need to make this grab from formula
+				$price = $quantity * $unit_price;
+				$store_id = $this->getStoreIdForSlug($this->columns[$store_column]);
+				
+				// Make an entry for each store column with a value in it 
+				$entries[] = array(
+					'item_id'    => $item_id,
+					'price'      => $price,
+					'quantity'   => $quantity,
+					'unit_price' => $unit_price,
+					'unit_id'    => $unit_id,
+					'store_id'   => $store_id,
+				);
+
+				$this->out("Adding $item for {$this->stores[$store_id]}");
+			}
+
+		}
+		
+		$entry->saveAll($entries);
 	}
 
 	private function getPriceSheet() {
@@ -295,7 +246,91 @@ class DataImportShell extends AppShell {
 	}
 
 	private function readCategories() {
-		$category = ClassRegistry::init('Category');
-		$this->categories = $category->find('list');
+		if(empty($this->categories)) {
+			$category = ClassRegistry::init('Category');
+			$this->categories = $category->find('list');
+		}
+	}
+
+	private function getCategoryIdForLabel($label) {
+		$this->readCategories();
+		$category_id = array_search($label, $this->categories);
+
+		if ($category_id === false) {
+			throw new Exception('Unknown Category: $label');
+		}
+
+		return $category_id;
+	}
+
+	private function readStores() {
+		if (empty($this->stores)) {
+			$store = ClassRegistry::init('Store');
+			$this->stores = $store->find('list');
+		}
+	}
+
+	private function readStoreSlugs() {
+		if (empty($this->store_slugs)) {
+			$store = ClassRegistry::init('Store');
+			$this->store_slugs = $store->find('list', array('fields' => array('id', 'slug')));
+		}
+	}
+
+	private function getStoreIdForSlug($slug) {
+		$this->readStoreSlugs();
+		$store_id = array_search($slug, $this->store_slugs);
+
+		if ($store_id === false) {
+			throw new Exception("Unknown Store: $slug");
+		}
+
+		return $store_id;
+	}
+
+	private function getColumnIndex($label) {
+		$column_index = array_search($label, $this->columns);
+
+		if($column_index === false) {
+			throw new Exception("Unknown column: $label");
+		}
+
+		return $column_index;
+	}
+
+	private function readUnits() {
+		if (empty($this->units)) {
+			$unit = ClassRegistry::init('Unit');
+			$this->units = $unit->find('list');
+		}
+	}
+
+	private function readItems() {
+		if (empty($this->items)) {
+			$item = ClassRegistry::init('Item');
+			$this->items = $item->find('list');
+		}
+	}
+
+	private function getItemIdForLabel($label) {
+		$this->readItems();
+		$item_id = array_search($label, $this->items);
+
+		if ($item_id === false) {
+			throw new Exception("Unknown Item: $label");
+		}
+
+		return $item_id;
+	}
+
+	private function getUnitIdForLabel($label) {
+		$this->readUnits();
+		$unit_id = array_search($label, $this->units);
+
+		if ($unit_id === false) {
+			return null;
+		}
+
+		return $unit_id;
 	}
 }
